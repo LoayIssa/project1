158e4196f576792bac4cfc1312a09e35c5863768
LoayIssa <issaloay680@gmail.com>
2021-04-23 16:24:41 +0300

    start project

---
+++ main.js
@@ -0,0 +1,185 @@
+
+let x = 0; 
+
+
+const squareOne = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch1");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch1") 
+   addO.innerHTML = "x"
+
+   }
+   const d =document.querySelector(".buxch1")
+   d.remove();
+   ++x;
+
+}
+
+const squareTwo = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch2");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch2") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch2")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareThree = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch3");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch3") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch3")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareFour = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch4");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch4") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch4")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareFive = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch5");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch5") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch5")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareSix = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch6");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch6") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch6")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareSeven = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch7");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch7") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch7")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareEight = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch8");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch8") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch8")
+   d.remove();
+   ++x;
+
+
+}
+
+const squareNine = ()=>{
+
+   if(x%2 === 0){
+
+    const addX = document.querySelector("#prch9");
+    addX.innerHTML = "o"
+   }
+    if(x%2 !== 0){
+   const addO = document.querySelector("#prch9") 
+   addO.innerHTML = "x"
+   }
+   const d =document.querySelector(".buxch9")
+   d.remove();
+   ++x;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
