9d1aaa5ab97eaa4f7b78d668bd7cfd2910c852d3
LoayIssa <issaloay680@gmail.com>
2021-04-24 12:50:04 +0300

    add if coondation  to determine and edit some css

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
 let x = 0; 
-
+let index1=""
+let index2=""
+let index3=""
+let index4=""
+let index5=""
+let index6=""
+let index7=""
+let index8=""
+let index9=""
 
 const squareOne = ()=>{
 
@@ -8,14 +16,18 @@ const squareOne = ()=>{
 
     const addX = document.querySelector("#prch1");
     addX.innerHTML = "o"
+    index1="o"
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch1") 
    addO.innerHTML = "x"
+   index1="x"
+
 
    }
    const d =document.querySelector(".buxch1")
    d.remove();
+   chek()
    ++x;
 
 }
@@ -26,13 +38,18 @@ const squareTwo = ()=>{
 
     const addX = document.querySelector("#prch2");
     addX.innerHTML = "o"
+    index2="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch2") 
    addO.innerHTML = "x"
+   index2="x"
+
    }
    const d =document.querySelector(".buxch2")
    d.remove();
+   chek()
    ++x;
 
 
@@ -44,13 +61,18 @@ const squareThree = ()=>{
 
     const addX = document.querySelector("#prch3");
     addX.innerHTML = "o"
+    index3="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch3") 
    addO.innerHTML = "x"
+   index3="x"
+
    }
    const d =document.querySelector(".buxch3")
    d.remove();
+   chek()
    ++x;
 
 
@@ -62,13 +84,17 @@ const squareFour = ()=>{
 
     const addX = document.querySelector("#prch4");
     addX.innerHTML = "o"
+    index4="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch4") 
    addO.innerHTML = "x"
+   index4="x"
    }
    const d =document.querySelector(".buxch4")
    d.remove();
+   chek()
    ++x;
 
 
@@ -80,13 +106,18 @@ const squareFive = ()=>{
 
     const addX = document.querySelector("#prch5");
     addX.innerHTML = "o"
+    index5="o"
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch5") 
    addO.innerHTML = "x"
+   index5="x"
+
    }
    const d =document.querySelector(".buxch5")
    d.remove();
+   chek()
+
    ++x;
 
 
@@ -98,13 +129,19 @@ const squareSix = ()=>{
 
     const addX = document.querySelector("#prch6");
     addX.innerHTML = "o"
+    index6="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch6") 
    addO.innerHTML = "x"
+   index6="x"
+
    }
    const d =document.querySelector(".buxch6")
    d.remove();
+   chek()
+
    ++x;
 
 
@@ -116,13 +153,19 @@ const squareSeven = ()=>{
 
     const addX = document.querySelector("#prch7");
     addX.innerHTML = "o"
+    index7="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch7") 
    addO.innerHTML = "x"
+   index7="x"
+
    }
    const d =document.querySelector(".buxch7")
    d.remove();
+   chek()
+
    ++x;
 
 
@@ -134,13 +177,19 @@ const squareEight = ()=>{
 
     const addX = document.querySelector("#prch8");
     addX.innerHTML = "o"
+    index8="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch8") 
    addO.innerHTML = "x"
+   index8="x"
+
    }
    const d =document.querySelector(".buxch8")
    d.remove();
+   chek()
+
    ++x;
 
 
@@ -152,15 +201,143 @@ const squareNine = ()=>{
 
     const addX = document.querySelector("#prch9");
     addX.innerHTML = "o"
+    index9="o"
+
    }
     if(x%2 !== 0){
    const addO = document.querySelector("#prch9") 
    addO.innerHTML = "x"
+   index9="x"
+
    }
    const d =document.querySelector(".buxch9")
    d.remove();
+   chek()
    ++x;
+}
 
+const chek = () => {
+   if (index1=== "x" && index2==="x" && index3==="x"){
+      document.getElementById("prch1").style.color="red";
+      document.getElementById("prch2").style.color="red";
+      document.getElementById("prch3").style.color="red";
+      winnerX();
+
+
+   }
+   if (index1=== "o" && index2==="o" && index3==="o"){
+      document.getElementById("prch1").style.color="red";
+      document.getElementById("prch2").style.color="red";
+      document.getElementById("prch3").style.color="red";
+      winnerO();
+   }
+
+   if (index4=== "x" && index5==="x" && index6==="x"){
+      document.getElementById("prch4").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch6").style.color="red";
+      winnerX();
+   }
+   if (index4=== "o" && index5==="o" && index6==="o"){
+      document.getElementById("prch4").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch6").style.color="red";
+      winnerO();
+   }
+   if (index7=== "x" && index8==="x" && index9==="x"){
+      document.getElementById("prch7").style.color="red";
+      document.getElementById("prch8").style.color="red";
+      document.getElementById("prch9").style.color="red";
+      winnerX();
+   }
+   if (index7=== "o" && index8==="o" && index9==="o"){
+      document.getElementById("prch7").style.color="red";
+      document.getElementById("prch8").style.color="red";
+      document.getElementById("prch9").style.color="red";
+      winnerO();
+   }
+
+   if (index1=== "x" && index4==="x" && index7==="x"){
+      document.getElementById("prch1").style.color="red";
+      document.getElementById("prch4").style.color="red";
+      document.getElementById("prch7").style.color="red";
+      winnerX();
+   }
+   if (index1=== "o" && index4==="o" && index7==="o"){
+      document.getElementById("prch1").style.color="red";
+      document.getElementById("prch4").style.color="red";
+      document.getElementById("prch7").style.color="red";
+      winnerO();
+   }
+   if (index2=== "x" && index5==="x" && index8==="x"){
+      document.getElementById("prch2").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch8").style.color="red";
+      winnerX();
+   }
+   if (index2=== "o" && index5==="o" && index8==="o"){
+      document.getElementById("prch2").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch8").style.color="red";
+      winnerO();
+   }
+
+   if (index3=== "x" && index6==="x" && index9==="x"){
+      document.getElementById("prch3").style.color="red";
+      document.getElementById("prch6").style.color="red";
+      document.getElementById("prch9").style.color="red";
+      winnerX();
+   }
+   if (index3=== "o" && index6==="o" && index9==="o"){
+      document.getElementById("prch3").style.color="red";
+      document.getElementById("prch6").style.color="red";
+      document.getElementById("prch9").style.color="red";
+      winnerO();
+   }
+
+   if (index1=== "x" && index5==="x" && index9==="x"){
+      document.getElementById("prch1").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch9").style.color="red";
+      winnerX();
+   }
+   if (index1=== "o" && index5==="o" && index9==="o"){
+      document.getElementById("prch1").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch9").style.color="red";
+      winnerO();
+   }
+   if (index3=== "x" && index5==="x" && index7==="x"){
+      document.getElementById("prch3").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch7").style.color="red";
+      winnerX();
+      }
+   if (index3=== "o" && index5==="o" && index7==="o"){
+      document.getElementById("prch3").style.color="red";
+      document.getElementById("prch5").style.color="red";
+      document.getElementById("prch7").style.color="red";
+      winnerO();
+   }
+}
+
+
+const winnerX =()=>{
+   document.getElementById("result").innerHTML = "winner x "
+   setTimeout(function(){
+
+      location.reload()
+    }, 5000);
+
+}
+
+const winnerO =()=>{
+   document.getElementById("result").innerHTML = "winner o "
+
+   setTimeout(function(){
+
+      location.reload()
+    }, 5000);
 
 }
 
